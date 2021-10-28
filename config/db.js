93d1ff246da6c5b32428d47f9068dc67f18c16c5
Jai <jpate661@my.centennialcollege.ca>
2021-10-27 20:29:04 -0400

    Finished

---
+++ config/db.js
@@ -0,0 +1,10 @@
+
+/*File Name - db.js
+Student Name - Jai Patel
+Student ID - 301170222
+Web App Name- comp229-jaipatel-middterm*/
+
+module.exports = {
+  AtlasDB:
+    "mongodb+srv://Jai:imbAK7H5K1PiOJqZ@mongodbserver.mz0ex.mongodb.net/books?retryWrites=true&w=majority",
+};
